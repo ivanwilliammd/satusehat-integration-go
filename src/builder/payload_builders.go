f524dc5ea195570c3cdc1bacc669e039a89d5375
Ivan William <ivanwilliam.md@gmail.com>
2026-08-28 10:35:11 +0700

    feat(builder): update Go builders with comprehensive methods and datatype integration

--- src/builder/payload_builders.go
+++ src/builder/payload_builders.go
@@ -1,103 +1,67 @@
 package builder
 
+import "github.com/ivanwilliammd/satusehat-integration-go/src/datatype"
+
 type BaseBuilder struct {
 	Data map[string]interface{}
 }
 
-func (b *BaseBuilder) ToMap() map[string]interface{} {
-	return b.Data
-}
+func (b *BaseBuilder) ToMap() map[string]interface{} { return b.Data }
+func (b *BaseBuilder) SetId(id string) { b.Data["id"] = id }
+func (b *BaseBuilder) SetActive(active bool) { b.Data["active"] = active }
 
 type PatientBuilder struct{ BaseBuilder }
 func NewPatient() *PatientBuilder {
 	return &PatientBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Patient"}}}
 }
-func (b *PatientBuilder) SetNik(nik string) *PatientBuilder {
-	b.Data["identifier"] = []map[string]interface{}{
-		{"system": "https://fhir.kemkes.go.id/id/nik", "value": nik},
+func (b *PatientBuilder) AddIdentifier(id datatype.Identifier) *PatientBuilder {
+	b.Data["identifier"] = append(b.GetSlice("identifier"), id.Value)
+	return b
+}
+func (b *PatientBuilder) SetGender(gender string) *PatientBuilder { b.Data["gender"] = gender; return b }
+func (b *PatientBuilder) SetBirthDate(dt string) *PatientBuilder { b.Data["birthDate"] = dt; return b }
+func (b *PatientBuilder) AddName(name datatype.HumanName) *PatientBuilder {
+	b.Data["name"] = append(b.GetSlice("name"), name.ToArray())
+	return b
+}
+func (b *PatientBuilder) AddAddress(addr datatype.Address) *PatientBuilder {
+	b.Data["address"] = append(b.GetSlice("address"), addr.ToArray())
+	return b
+}
+func (b *PatientBuilder) AddTelecom(tc datatype.ContactPoint) *PatientBuilder {
+	b.Data["telecom"] = append(b.GetSlice("telecom"), tc.ToArray())
+	return b
+}
+
+func (b *BaseBuilder) GetSlice(key string) []interface{} {
+	if val, ok := b.Data[key].([]interface{}); ok {
+		return val
 	}
-	return b
-}
-func (b *PatientBuilder) SetName(name string) *PatientBuilder {
-	b.Data["name"] = []map[string]interface{}{{"use": "official", "text": name}}
-	return b
-}
-func (b *PatientBuilder) SetGender(gender string) *PatientBuilder {
-	b.Data["gender"] = gender
-	return b
-}
-func (b *PatientBuilder) SetBirthDate(birthDate string) *PatientBuilder {
-	b.Data["birthDate"] = birthDate
-	return b
+	return []interface{}{}
 }
 
 type PractitionerBuilder struct{ BaseBuilder }
 func NewPractitioner() *PractitionerBuilder {
 	return &PractitionerBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Practitioner"}}}
 }
-func (b *PractitionerBuilder) SetNik(nik string) *PractitionerBuilder {
-	b.Data["identifier"] = []map[string]interface{}{
-		{"system": "https://fhir.kemkes.go.id/id/nik", "value": nik},
-	}
-	return b
-}
-func (b *PractitionerBuilder) SetName(name string) *PractitionerBuilder {
-	b.Data["name"] = []map[string]interface{}{{"use": "official", "text": name}}
-	return b
-}
 
 type OrganizationBuilder struct{ BaseBuilder }
 func NewOrganization() *OrganizationBuilder {
 	return &OrganizationBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Organization"}}}
 }
-func (b *OrganizationBuilder) SetId(id string) *OrganizationBuilder {
-	b.Data["id"] = id
-	return b
-}
-func (b *OrganizationBuilder) SetName(name string) *OrganizationBuilder {
-	b.Data["name"] = name
-	return b
-}
+func (b *OrganizationBuilder) SetName(name string) *OrganizationBuilder { b.Data["name"] = name; return b }
 
 type LocationBuilder struct{ BaseBuilder }
 func NewLocation() *LocationBuilder {
 	return &LocationBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Location"}}}
-}
-func (b *LocationBuilder) SetName(name string) *LocationBuilder {
-	b.Data["name"] = name
-	return b
-}
-func (b *LocationBuilder) SetManagingOrganization(orgRef string) *LocationBuilder {
-	b.Data["managingOrganization"] = map[string]interface{}{"reference": orgRef}
-	return b
 }
 
 type EncounterBuilder struct{ BaseBuilder }
 func NewEncounter() *EncounterBuilder {
 	return &EncounterBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Encounter"}}}
 }
-func (b *EncounterBuilder) SetStatus(status string) *EncounterBuilder {
-	b.Data["status"] = status
-	return b
-}
-func (b *EncounterBuilder) SetSubject(patientRef string, display string) *EncounterBuilder {
-	b.Data["subject"] = map[string]interface{}{"reference": patientRef, "display": display}
-	return b
-}
 
 type ConditionBuilder struct{ BaseBuilder }
 func NewCondition() *ConditionBuilder {
 	return &ConditionBuilder{BaseBuilder: BaseBuilder{Data: map[string]interface{}{"resourceType": "Condition"}}}
-}
-func (b *ConditionBuilder) SetClinicalStatus(code string) *ConditionBuilder {
-	b.Data["clinicalStatus"] = map[string]interface{}{
-		"coding": []map[string]interface{}{
-			{"system": "http://terminology.hl7.org/CodeSystem/condition-clinical", "code": code},
-		},
-	}
-	return b
-}
-func (b *ConditionBuilder) SetSubject(patientRef string, display string) *ConditionBuilder {
-	b.Data["subject"] = map[string]interface{}{"reference": patientRef, "display": display}
-	return b
 }
